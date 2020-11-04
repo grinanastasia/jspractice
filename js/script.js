a1904151d26f0687587b47ddbf29de9739da3298
Anastasia Grin <grin2008.96@mail.ru>
2020-11-04 21:59:10 +0300

    add let

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 "use strict";
+/* jshint browser: true */
+/* jshint devel: true */
+let startBtn = document.getElementById("start"),
+  bugetValue = document.getElementsByClassName("budget-value")[0],
+  daybudgetValue = document.getElementsByClassName("daybudget-value")[0],
+  levelValue = document.getElementsByClassName("level-value")[0],
+  expensesValue = document.getElementsByClassName("expenses-value")[0],
+  optionalexpensesValue = document.getElementsByClassName(
+    "optionalexpenses-value"
+  )[0],
+  incomeValue = document.getElementsByClassName("income-value")[0],
+  monthsavingsValue = document.getElementsByClassName("monthsavings-value")[0],
+  yearsavingsValue = document.getElementsByClassName("yearsavings-value")[0],
+  expensesItem = document.getElementsByClassName("expenses-item"),
+  expensesBtn = document.getElementsByTagName("button")[0],
+  optionalBtn = document.getElementsByTagName("button")[1],
+  countBtn = document.getElementsByTagName("button")[2],
+  optionalExpensesItem = document.querySelectorAll(".optionalexpenses-item"),
+  incomeItem = document.querySelector(".choose-income"),
+  checkSavings = document.querySelector("#savings"),
+  sumValue = document.querySelector(".choose-sum"),
+  percentValue = document.querySelector(".choose-percent"),
+  yearValue = document.querySelector(".year-value"),
+  monthValue = document.querySelector(".month-value"),
+  dayValue = document.querySelector(".day-value");
+
 let money, time;
 function start() {
   /* всплывающеее окно на сайте с вопросом */
